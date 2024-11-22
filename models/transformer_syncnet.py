0263a9e9de4e3190ff2e32bd64a0465c0f87f5c6
Eddy Ma <ocean_ltd@hotmail.com>
2024-11-21 19:34:54 -0800

    bigger syncnet

--- models/transformer_syncnet.py
+++ models/transformer_syncnet.py
@@ -16,45 +16,37 @@ class TransformerSyncnet(nn.Module):
         '''
         self.face_encoder = nn.Sequential(
             
-            Conv2d(15, 32, kernel_size=3, stride=1, padding=1), #192x192, 1+(3−1)×1=3
-            Conv2d(32, 32, kernel_size=3, stride=1, padding=1, residual=True), #192x192, 3+(3−1)×1=5
+            Conv2d(15, 128, kernel_size=3, stride=1, padding=1), #192x192, 1+(3−1)×1=3
+            Conv2d(128, 128, kernel_size=3, stride=1, padding=1, residual=True), #192x192, 3+(3−1)×1=5
             
-            Conv2d(32, 64, kernel_size=3, stride=2, padding=1), #96x96, 7
-            Conv2d(64, 64, kernel_size=3, stride=1, padding=1, residual=True), # 9
-            Conv2d(64, 64, kernel_size=3, stride=1, padding=1, residual=True), # 11
+            Conv2d(128, 256, kernel_size=3, stride=2, padding=1), #96x96, 7
+            Conv2d(256, 256, kernel_size=3, stride=1, padding=1, residual=True), # 9
+            Conv2d(256, 256, kernel_size=3, stride=1, padding=1, residual=True), # 11
 
-            Conv2d(64, 128, kernel_size=3, stride=(1, 2), padding=1), #94x47, 13
-            Conv2d(128, 128, kernel_size=3, stride=1, padding=1, residual=True), #94x47, 15
-            Conv2d(128, 128, kernel_size=3, stride=1, padding=1, residual=True), #94x47, 17
+            Conv2d(256, 512, kernel_size=3, stride=(1, 2), padding=1), #94x47, 13
+            Conv2d(512, 512, kernel_size=3, stride=1, padding=1, residual=True), #94x47, 15
+            Conv2d(512, 512, kernel_size=3, stride=1, padding=1, residual=True), #94x47, 17
 
-            Conv2d(128, 256, kernel_size=3, stride=2, padding=1), # 47x24, 19
-            Conv2d(256, 256, kernel_size=3, stride=1, padding=1, residual=True), # 47x24, 21
-            Conv2d(256, 256, kernel_size=3, stride=1, padding=1, residual=True), # 47x24, 23
+            Conv2d(512, 1024, kernel_size=3, stride=2, padding=1), # 47x24, 19
+            Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1, residual=True), # 47x24, 21
 
-            Conv2d(256, 256, kernel_size=3, stride=2, padding=1), # 24x 12, 25
-            Conv2d(256, 256, kernel_size=3, stride=1, padding=1, residual=True), # 24x 12, 27
-            Conv2d(256, 256, kernel_size=3, stride=1, padding=1, residual=True), # 24x 12, 29
-
-            Conv2d(256, 512, kernel_size=3, stride=1, padding=1), # 24x 12, 31
-            Conv2d(512, 512, kernel_size=3, stride=1, padding=1, residual=True), # 24x 12, 33
-            Conv2d(512, 512, kernel_size=3, stride=1, padding=1, residual=True), # 24x 12, 35
+            Conv2d(1024, 512, kernel_size=3, stride=2, padding=1), # 24x 12, 25
+            Conv2d(512, 512, kernel_size=3, stride=1, padding=1, residual=True), # 24x 12, 27
+            Conv2d(512, 512, kernel_size=3, stride=1, padding=1, residual=True), # 24x 12, 29
 
             Conv2d(512, 256, kernel_size=3, stride=2, padding=1), #12x6, 37
             Conv2d(256, 256, kernel_size=3, stride=1, padding=1, residual=True), #12x6, 39
             Conv2d(256, 256, kernel_size=3, stride=1, padding=1, residual=True), #12x6, 41
 
-            Conv2d(256, 256, kernel_size=3, stride=(2,1), padding=1), #6x6, 43
-            Conv2d(256, 256, kernel_size=3, stride=1, padding=1, residual=True), # 45
-            Conv2d(256, 256, kernel_size=3, stride=1, padding=1, residual=True), # 47
+            Conv2d(256, 128, kernel_size=3, stride=(2,1), padding=1), #6x6, 43
+            Conv2d(128, 128, kernel_size=3, stride=1, padding=1, residual=True), # 45
+            Conv2d(128, 128, kernel_size=3, stride=1, padding=1, residual=True), # 47
 
-            Conv2d(256, 128, kernel_size=3, stride=1, padding=1), #6x6, 49
-            Conv2d(128, 128, kernel_size=3, stride=1, padding=1, residual=True), #6x6, 51
+            Conv2d(128, 64, kernel_size=3, stride=1, padding=1), #6x6, 49
+            Conv2d(64, 64, kernel_size=3, stride=1, padding=1, residual=True), #6x6, 51
 
-            Conv2d(128, 128, kernel_size=3, stride=1, padding=1), #6x6, 53
-            Conv2d(128, 128, kernel_size=3, stride=1, padding=1, residual=True), #3x3, 55
-
-            Conv2d(128, 64, kernel_size=3, stride=2, padding=1), #3x3, 57
-            Conv2d(64, 64, kernel_size=3, stride=1, padding=1, residual=True), #3x3, 59
+            Conv2d(64, 64, kernel_size=3, stride=2, padding=1), #3x3, 53
+            Conv2d(64, 64, kernel_size=3, stride=1, padding=1, residual=True), #3x3, 57
             
             )
 
@@ -89,14 +81,14 @@ class TransformerSyncnet(nn.Module):
 
 
         self.transformer_encoder = nn.TransformerEncoder(
-            nn.TransformerEncoderLayer(d_model=512, nhead=num_heads, dropout=0.1),
+            nn.TransformerEncoderLayer(d_model=768, nhead=num_heads, dropout=0.1),
             num_layers=num_encoder_layers
         )
 
         self.relu = nn.LeakyReLU(0.01, inplace=True)
-        self.fc1 = nn.Linear(384, 256) 
-        self.fc2 = nn.Linear(512, 256) 
-        self.fc3 = nn.Linear(512, 2) 
+        #self.fc1 = nn.Linear(384, 256) 
+        self.fc2 = nn.Linear(512, 384) 
+        self.fc3 = nn.Linear(768, 2) 
         
 
     def forward(self, face_embedding, audio_embedding):
@@ -109,7 +101,7 @@ class TransformerSyncnet(nn.Module):
         audio_embedding = audio_embedding.view(audio_embedding.size(0), -1)
         face_embedding = face_embedding.view(face_embedding.size(0), -1)
 
-        face_embedding = self.fc1(face_embedding)
+        #face_embedding = self.fc1(face_embedding)
         audio_embedding = self.fc2(audio_embedding)
 
         # normalise them
