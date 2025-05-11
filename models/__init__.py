cead6c244058b74718743bfc51c47536251a5f5a
Eddy Ma <ocean_ltd@hotmail.com>
2025-05-11 16:21:58 -0700

    add import

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 from .resunet import ResUNet
 from .resunet384 import ResUNet384
 from .resunet384_v2 import ResUNet384V2, FaceEnhancer
-from .resunet384_v3 import ResUNet384V3
+from .resunet384_v3 import ResUNet384V3, cosine_noise_schedule
 from .transformer_syncnet import TransformerSyncnet
 from .transformer_syncnet_v2 import TransformerSyncnetV2
 from .transformer_ressyncnet import TransformerResSyncnet
