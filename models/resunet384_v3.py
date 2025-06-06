a7203c4dd6e76986dcdcdf33f1c1a5f3b817e59e
Eddy Ma <ocean_ltd@hotmail.com>
2025-06-06 15:28:10 -0700

    cleanup

--- models/resunet384_v3.py
+++ models/resunet384_v3.py
@@ -84,11 +84,6 @@ class ResUNet384V3(nn.Module):
         
         self.face_encoder5 = self.construct_encoder_layers(3, 128, 128, 1, True)
         self.fe_down5 = self.construct_encoder_layers(3, 128, 128, 2)
-        
-        # self.face_transformer_encoder = nn.TransformerEncoder(
-        #     nn.TransformerEncoderLayer(d_model=128, nhead=8, dropout=0.1, activation='gelu'),
-        #     num_layers=2
-        # )
 
         # --- Audio encoder ---
         self.audio_encoder1 = nn.Sequential(
@@ -284,31 +279,16 @@ class ResUNet384V3(nn.Module):
 
         face3 = self.face_encoder3(fed2_concatenated)
         fed3 = self.fe_down3(face3)
-        
-        
 
         face4 = self.face_encoder4(fed3)
         fed4 = self.fe_down4(fed3 + face4)       
         
         face5 = self.face_encoder5(fed4)
         fed5 = self.fe_down5(fed4 + face5)       
-        
                  
         fed5 = self.face_pos_encoder(fed5)
         
-        # FB, _, H, W = fed5.shape
-        
-        # face_flatten = fed5.view(FB, 128, -1).permute(0, 2, 1)
-        
-        # face_output = self.face_transformer_encoder(face_flatten)
-
-        # face_output = face_flatten + face_output
-        
-        # face_swapped_back = face_output.permute(0, 2, 1).view(FB, 128, 12, 12)  # Shape: [5, 512, 144]
-
-        #self.save_sample_images(face_swapped_back, 10)
         combined = torch.cat([fed5, audio_embedding1], dim=1)
-        
         
         bottlenet = self.bottlenet(combined)
         
