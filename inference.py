8adfa3361a14f3f97089931c5f2afc60962c6d99
Eddy Ma <ocean_ltd@hotmail.com>
2025-06-19 16:09:15 -0700

    support v4

--- inference.py
+++ inference.py
@@ -6,7 +6,7 @@ import json, subprocess, random, string
 from tqdm import tqdm
 from glob import glob
 import torch, face_detection
-from models import ResUNet384V2, ResUNet384V3, cosine_noise_schedule
+from models import ResUNet384V2, ResUNet384V3, ResUNet384V4, cosine_noise_schedule
 from realesrgan import RealESRGANer
 from basicsr.archs.rrdbnet_arch import RRDBNet
 from PIL import Image
@@ -80,7 +80,7 @@ parser.add_argument('--use_esrgan', default=False, type=str2bool)
 
 parser.add_argument('--iteration', type=int, help='Number of iteration to inference', default=2)
 
-parser.add_argument('--version', type=str, help='The version of the model', default='V3')
+parser.add_argument('--version', type=str, help='The version of the model', default='V4')
 
 args = parser.parse_args()
 args.img_size = 384
@@ -340,6 +340,8 @@ def _load(checkpoint_path):
 def load_model(path, lora_path=None):
   if args.version == 'V3':
     model = ResUNet384V3()
+  elif args.version == 'V4':
+    model = ResUNet384V4()
   else:
     model = ResUNet384V2()
   print("Load checkpoint from: {}".format(path))
