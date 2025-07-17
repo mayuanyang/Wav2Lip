eeff862dd8b8c172b4d3b87ff61700be092ebba9
Eddy Ma <ocean_ltd@hotmail.com>
2025-07-16 19:53:03 -0700

    add spatial attention

---
+++ models/spatial_attention.py
@@ -0,0 +1,19 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+        assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
+        padding = 3 if kernel_size == 7 else 1
+        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x_att = torch.cat([avg_out, max_out], dim=1)
+        x_att = self.conv1(x_att)
+        x_att = self.sigmoid(x_att)
+        return x + x_att
